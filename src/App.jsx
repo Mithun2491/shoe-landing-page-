a731346254a597149801f7af93d6410557d8b708
Mithun2491 <145950507+Mithun2491@users.noreply.github.com>
2024-02-10 10:22:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import './Navigation'
+import Navigation from './Navigation'
+import HeroSection from './hero'
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+       <Navigation/>
+       <HeroSection/>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
